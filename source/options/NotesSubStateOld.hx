d535eb6b48fef131e87ab90d755b8c4341035ec5
ArkoseLabsOfficial <28umutmehmet28@gmail.com>
2026-01-24 20:59:18 +0000

    *sigh*

--- source/options/NotesSubStateOld.hx
+++ source/options/NotesSubStateOld.hx
@@ -167,7 +167,7 @@ class NotesSubStateOld extends MusicBeatSubstate
 				} else if(controls.UI_RIGHT_P) {
 					updateValue(1);
 					FlxG.sound.play(Paths.sound('scrollMenu'));
-				} else if(controls.RESET mobileButtonJustPressed('C')) {
+				} else if(controls.RESET || mobileButtonJustPressed('C')) {
 					resetValue(curSelected, typeSelected);
 					FlxG.sound.play(Paths.sound('scrollMenu'));
 				}
@@ -208,7 +208,7 @@ class NotesSubStateOld extends MusicBeatSubstate
 				changeType(1);
 				FlxG.sound.play(Paths.sound('scrollMenu'));
 			}
-			if(controls.RESET mobileButtonJustPressed('C')) {
+			if(controls.RESET || mobileButtonJustPressed('C')) {
 				for (i in 0...3) {
 					resetValue(curSelected, i);
 				}
