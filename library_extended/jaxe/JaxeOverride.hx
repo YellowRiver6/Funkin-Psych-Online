34e3a8010acfa7c1ed2f556649a2fc148867c2ce
ArkoseLabsOfficial <28umutmehmet28@gmail.com>
2026-04-02 20:07:11 +0000

    uhhhh...

--- library_extended/jaxe/JaxeOverride.hx
+++ library_extended/jaxe/JaxeOverride.hx
@@ -7,7 +7,21 @@ import haxe.macro.ExprTools;
 class JaxeOverride {
 	public static function build():Array<Field> {
 		var fields = Context.getBuildFields();
+		var localClass = Context.getLocalClass().get();
 		var newFields:Array<Field> = [];
+
+		// Check if any parent function in the class.
+		function hasFieldInParents(fieldName:String):Bool {
+			var cl = localClass.superClass;
+			while (cl != null) {
+				var t = cl.t.get();
+				for (f in t.fields.get()) {
+					if (f.name == fieldName) return true;
+				}
+				cl = t.superClass;
+			}
+			return false;
+		}
 
 		for (field in fields) {
 			if (field.name.indexOf("__") == 0 || field.name == "hget" || field.name == "hset") continue; // Skip certain fields to get HScript Improved and Jaxe at the same time.
@@ -15,12 +29,24 @@ class JaxeOverride {
 			switch (field.kind) {
 				case FFun(f):
 					var name = field.name;
+					//makes if() much easier.
+					var isInline = field.access.indexOf(AInline) != -1;
+					var isStatic = field.access.indexOf(AStatic) != -1;
+					var isPrivate = field.access.indexOf(APrivate) != -1;
+					var isOverride = field.access.indexOf(AOverride) != -1;
+					var isDynamic = field.access.indexOf(ADynamic) != -1;
 
-					if (field.access.indexOf(AOverride) != -1 && field.access.indexOf(AInline) == -1 && field.access.indexOf(APrivate) == -1) {
+					if (isOverride && !isInline && !isPrivate) {
 						var superName = "super_" + name;
 						var scriptedName = "scripted_" + name;
 						var args = f.args;
 						var callArgs = [for (arg in args) macro $i{arg.name}];
+
+						var superAccess:Array<Access> = [APublic];
+						if (hasFieldInParents(superName)) superAccess.push(AOverride);
+
+						var scriptedAccess:Array<Access> = [APublic, ADynamic];
+						if (hasFieldInParents(scriptedName)) scriptedAccess.push(AOverride);
 
 						function patchSuperCalls(e:Expr):Expr {
 							if (e == null) return null;
@@ -51,7 +77,7 @@ class JaxeOverride {
 
 						newFields.push({
 							name: superName,
-							access: [APublic],
+							access: superAccess,
 							kind: FFun({
 								args: args,
 								ret: f.ret,
@@ -62,7 +88,7 @@ class JaxeOverride {
 
 						newFields.push({
 							name: scriptedName,
-							access: [APublic, ADynamic],
+							access: scriptedAccess,
 							kind: FFun({
 								args: args,
 								ret: f.ret,
@@ -75,8 +101,8 @@ class JaxeOverride {
 							$i{scriptedName}($a{callArgs});
 						};
 					}
-					else if (field.access.indexOf(AStatic) == -1 && field.access.indexOf(AInline) == -1 && name != "new") {
-						if (field.access.indexOf(ADynamic) == -1) field.access.push(ADynamic);
+					else if (!isStatic && !isInline && name != "new") {
+						if (!isDynamic) field.access.push(ADynamic);
 						//if (field.access.indexOf(APublic) == -1) field.access.push(APublic);
 					}
 				default:
--- library_extended/jaxe/JaxeScript.hx
+++ library_extended/jaxe/JaxeScript.hx
@@ -14,10 +14,10 @@ class JaxeScript {
 	private static inline var COLOR_YELLOW = "\x1b[33m";
 	private static inline var COLOR_RESET = "\x1b[0m";
 
-	public function new(sourceCode:String, scriptName:String = "Unnamed.java") {
+	public function new(sourceCode:String, scriptName:String = "Script.java") {
 		this.sourceCode = sourceCode;
 		this.scriptName = scriptName;
-
+		
 		this.interp = new JaxeInterp();
 		this.parser = new JaxeParser();
 		this.interp.scriptName = this.scriptName;
@@ -40,7 +40,11 @@ class JaxeScript {
 		interp.scriptObject = parent;
 	}
 
-	public function run() {
+	/**
+	 * Runs the script. If the script contains a class matching the filename, 
+	 * it creates an instance of it and returns the native Haxe object.
+	 */
+	public function run():Dynamic {
 		try {
 			ast = parser.parseString(sourceCode, scriptName);
 			interp.execute(ast);
@@ -51,23 +55,50 @@ class JaxeScript {
 			}
 
 			if (interp.scriptClasses.exists(targetClassName)) {
+				// Instantiate the class and return the native object
 				var mainInstance = interp.expr(ENew(targetClassName, [], []));
 				interp.extendedObject = mainInstance; 
+				return mainInstance; 
 			} else {
 				if (interp.locals.exists(targetClassName)) interp.call(targetClassName, []);
 				else if (interp.locals.exists("new")) interp.call("new", []);
 				else if (interp.locals.exists("main")) interp.call("main", []);
+				return this;
 			}
 		} catch (e:Dynamic) {
 			handleError(Std.string(e), parser.lastLine, parser.lastCol, e, scriptName);
+			return null;
 		}
 	}
+
+	#if sys
+	/**
+	 * Reads a file, parses the script, instantiates its main class, and returns it.
+	 * Allows seamless switching to scripted classes directly from Haxe.
+	 */
+	public static function loadClass(filePath:String):Dynamic {
+		if (!sys.FileSystem.exists(filePath)) {
+			handleError('Script file not found: $filePath', 0, 0, null, filePath);
+			return null;
+		}
+		
+		var content = sys.io.File.getContent(filePath);
+		var fileName = filePath;
+		
+		// Extract just the filename (e.g. "MyGameMenu.java" from "assets/scripts/MyGameMenu.java")
+		if (fileName.indexOf("/") != -1) fileName = fileName.split("/").pop();
+		if (fileName.indexOf("\\") != -1) fileName = fileName.split("\\").pop();
+		
+		var script = new JaxeScript(content, fileName);
+		return script.run();
+	}
+	#end
 
 	public static function handleError(msg:String, line:Int = 0, col:Int = 0, e:Dynamic = null, scriptName:String = "Unknown", isWarning:Bool = false) {
 		var color = isWarning ? COLOR_YELLOW : COLOR_RED;
 		var prefix = isWarning ? "[Jaxe Warning]" : "[Jaxe Error]";
 		var output = '$color$prefix $scriptName:$line:$col - $msg$COLOR_RESET';
-
+		
 		#if sys
 		Sys.println(output);
 		#else
