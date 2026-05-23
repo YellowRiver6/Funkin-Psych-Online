1b5e8ee1b440c89b694893857bcb6b7d874f3c62
qyovo438 <3501622460@qq.com>
2026-05-24 00:15:36 +0800

    Update InputText.hx

--- source/online/objects/InputText.hx
+++ source/online/objects/InputText.hx
@@ -2,8 +2,11 @@ package online.objects;
 
 import online.gui.sidebar.SideUI;
 import flixel.addons.ui.FlxInputText;
-import openfl.text.TextField;
 
+/**
+ * 聊天输入框组件
+ * 用于游戏内聊天输入
+ */
 class InputText extends FlxInputText {
     public function new(x:Float, y:Float, width:Float, onEnter:(text:String)->Void) {
         super(x, y, Std.int(width));
@@ -12,12 +15,9 @@ class InputText extends FlxInputText {
 		fieldBorderColor = FlxColor.TRANSPARENT;
 		caretColor = FlxColor.WHITE;
 
-        // 关键：启用系统输入法（IME），支持中文
-        textField.imeEnabled = true;
-        textField.type = TextFieldType.INPUT;
-
         var prevText:String = '';
 		callback = (text, action) -> {
+            // 禁止在UI打开时输入
 			if (SideUI.instance != null && SideUI.instance.active) {
                 this.text = prevText;
                 return;
@@ -25,6 +25,7 @@ class InputText extends FlxInputText {
 
             prevText = text;
 
+            // 按下回车时提交消息
             if (action == FlxInputText.ENTER_ACTION) {
 				hasFocus = false;
 				onEnter(text);
@@ -35,6 +36,7 @@ class InputText extends FlxInputText {
     override function update(elapsed) {
         super.update(elapsed);
 
+		// 按ESC或点击外部失去焦点
 		if (hasFocus && (FlxG.keys.justPressed.ESCAPE || (FlxG.mouse.justPressed && !FlxG.mouse.overlaps(this)))) {
             hasFocus = false;
         }
