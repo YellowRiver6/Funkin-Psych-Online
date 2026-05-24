83b6e3279814b42efb2fda8e6be48f0769936164
qyovo438 <3501622460@qq.com>
2026-05-24 18:53:08 +0800

    Update InputText.hx

--- source/online/objects/InputText.hx
+++ source/online/objects/InputText.hx
@@ -4,10 +4,8 @@ import online.gui.sidebar.SideUI;
 import flixel.addons.ui.FlxInputText;
 import lime.ui.Keyboard;
 import lime.events.TextEvent;
-import openfl.events.KeyboardEvent;
 
 class InputText extends FlxInputText {
-    public var hasFocus:Bool = false;
 
     public function new(x:Float, y:Float, width:Float, onEnter:(text:String)->Void) {
         super(x, y, Std.int(width));
@@ -25,19 +23,18 @@ class InputText extends FlxInputText {
             prevText = text;
 
             if (action == FlxInputText.ENTER_ACTION) {
-                hasFocus = false;
                 onEnter(text);
+                this.text = "";
             }
         };
     }
 
-    override function set_hasFocus(value:Bool):Bool {
-        if (value == true) {
-            // 安卓 + Windows 通用 IME
+    override function set_focus(value:Bool):Bool {
+        if (value) {
             Keyboard.startTextInput();
             Keyboard.setTextInputRect(new lime.geom.Rectangle(x, y, width, height));
-            
             addEventListener(TextEvent.TEXT_INPUT, onTextInput);
+            
             #if android
             FlxG.stage.window.textInputEnabled = true;
             #end
@@ -49,10 +46,9 @@ class InputText extends FlxInputText {
             FlxG.stage.window.textInputEnabled = false;
             #end
         }
-        return super.set_hasFocus(value);
+        return super.set_focus(value);
     }
 
-    // 核心：接收中文、日文、韩文等所有输入法
     function onTextInput(e:TextEvent):Void {
         if (e.text != null && e.text != "") {
             this.text += e.text;
@@ -62,8 +58,8 @@ class InputText extends FlxInputText {
     override function update(elapsed) {
         super.update(elapsed);
 
-        if (hasFocus && (FlxG.keys.justPressed.ESCAPE || (FlxG.mouse.justPressed && !FlxG.mouse.overlaps(this)))) {
-            hasFocus = false;
+        if (focus && (FlxG.keys.justPressed.ESCAPE || (FlxG.mouse.justPressed && !FlxG.mouse.overlaps(this)))) {
+            focus = false;
         }
     }
 }
