c5697683c7d997ab70df0d515bb5d78e5789908c
qyovo438 <3501622460@qq.com>
2026-05-30 23:10:35 +0800

    Update MobileOptionsSubState.hx

--- source/mobile/options/MobileOptionsSubState.hx
+++ source/mobile/options/MobileOptionsSubState.hx
@@ -25,7 +25,7 @@ class MobileOptionsSubState extends BaseOptionsMenu {
 		HitboxTypes = Mods.mergeAllTextsNamed('mobile/Hitbox/HitboxModes/hitboxModeList.txt');
 
 		option = new Option('MobilePad Opacity',
-			'Selects the opacity for the mobile buttons (careful not to put it at 0 and lose track of your buttons).', 'mobilePadAlpha', 'percent');
+			'设置移动端按钮的不透明度（注意不要将数值设为 0，否则会完全看不到按钮）', 'mobilePadAlpha', 'percent');
 		option.scrollSpeed = 1;
 		option.minValue = 0.001;
 		option.maxValue = 1;
@@ -38,7 +38,7 @@ class MobileOptionsSubState extends BaseOptionsMenu {
 		addOption(option);
 
 		var option:Option = new Option('Extra Controls',
-			'Allow Extra Controls',
+			'允许显示附加按钮（躲避）数量',
 			'extraKeys',
 			'int');
 		option.scrollSpeed = 1;
@@ -49,7 +49,7 @@ class MobileOptionsSubState extends BaseOptionsMenu {
 		addOption(option);
 
 		option = new Option('Extra Control Location',
-			'Choose Extra Control Location',
+			'选择附加按钮（躲避）位置',
 			'hitboxLocation',
 			'string',
 			['Bottom', 'Top', 'Middle']
@@ -58,7 +58,7 @@ class MobileOptionsSubState extends BaseOptionsMenu {
 		
 		//HitboxTypes.insert(0, "Classic");
 		option = new Option('Hitbox Mode',
-			'Choose your Hitbox Style!',
+			'选择你的碰撞框样式！',
 			'hitboxMode',
 			'string',
 			HitboxTypes
@@ -66,7 +66,7 @@ class MobileOptionsSubState extends BaseOptionsMenu {
 		addOption(option);
 		
 		option = new Option('Hitbox Design',
-			'Choose how your hitbox should look like.',
+			'自定义点击按键区域的外观样式',
 			'hitboxType',
 			'string',
 			['Gradient', 'No Gradient' , 'No Gradient (Old)']
@@ -75,18 +75,18 @@ class MobileOptionsSubState extends BaseOptionsMenu {
 
 		option = new Option('Hitbox Hint',
 			'Hitbox Hint',
-			'hitboxHint',
+			'点击按钮区域提示',
 			'bool');
 		addOption(option);
 
 		option = new Option('V Slice Controls',
-			'If checked, The game\'s control will be like original Friday Night Funkin\': Mobile.\n(WARNING: This Option can break the some mechanics, please use for simple mods)',
+			'勾选此选项后，游戏操控逻辑将还原原版《周五夜放克》手机版的操作模式\n(警告：该选项可能会导致部分游戏机制失效，仅建议简易模组使用)',
 			'ogGameControls',
 			'bool');
 		addOption(option);
 
 		option = new Option('Hitbox Opacity',
-			'Selects the opacity for the hitbox buttons.',
+			'调节点击按钮区域的不透明度',
 			'hitboxAlpha',
 			'percent'
 		);
@@ -99,7 +99,7 @@ class MobileOptionsSubState extends BaseOptionsMenu {
 
 		#if mobile
 		option = new Option('Wide Screen Mode',
-			'If checked, The game will stetch to fill your whole screen. (WARNING: Can result in bad visuals & break some mods that resizes the game/cameras)',
+			'勾选后，游戏画面将拉伸铺满整个屏幕\n(警告：该功能会造成画面显示效果变差，还可能使部分调整游戏画面、镜头尺寸的模组出现故障)',
 			'wideScreen', 'bool');
 		option.onChange = () -> ScreenUtil.wideScreen.enabled = ClientPrefs.data.wideScreen;
 		addOption(option);
@@ -107,7 +107,7 @@ class MobileOptionsSubState extends BaseOptionsMenu {
 
 		#if android
 		option = new Option('Storage Type',
-			'Which folder Psych Online should use?',
+			'选择 Psych Online 所保存数据的文件夹',
 			'storageType',
 			'string',
 			storageTypes
