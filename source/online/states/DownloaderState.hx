99b10f0422f909457bf5fbfeb9bea7f16ddb1a4d
qyovo438 <3501622460@qq.com>
2026-05-31 16:04:57 +0800

    Update DownloaderState.hx

--- source/online/states/DownloaderState.hx
+++ source/online/states/DownloaderState.hx
@@ -173,10 +173,10 @@ class DownloaderState extends MusicBeatState {
 			return;
 
 		if (mods == null)
-			err = "未找到模组！";
+			err = "请前往https://psychcn.online/mods下载模组！";
 
 		if (err != null) {
-			pageInfo.text = "错误：" + err;
+			pageInfo.text = "警告！" + err;
 			return;
 		}
 
