5a0897701262fa6ed012aad37ad8ba2107d29f7b
YellowRiver6 <130995473+YellowRiver6@users.noreply.github.com>
2026-06-06 23:21:41 +0800

    chat win

--- source/online/states/RoomState.hx
+++ source/online/states/RoomState.hx
@@ -24,6 +24,9 @@ import states.FreeplayState;
 import states.ModsMenuState;
 import openfl.utils.Assets as OpenFlAssets;
 import openfl.Lib;
+#if windows
+import externs.WinAPI;
+#end
 
 #if lumod
 @:build(lumod.LuaScriptClass.build())
@@ -385,6 +388,11 @@ class RoomState extends MusicBeatState /*#if interpret implements interpret.Inte
 					return true;
 				case "help":
 					ChatBox.addMessage("> 房间指令: /pa <anmi>, /results, /restage");
+				#if windows
+				case "chat":
+					var msg = WinAPI.prompt("输入聊天内容","");
+					GameClient.send("chat", msg);
+				#end
 			}
 			return false;
 		});
